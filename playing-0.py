ce686d56a70d8510da56bc8f39b63abea3b72c37
Chris <cdyoder@ncsu.edu>
2021-12-19 14:24:30 -0500

    Dev work - sunday
    Continued to bring over old functions, update comments, etc.

---
+++ playing-0.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 19 10:39:47 2021
+@author: Christopher D. Yoder
+
+Preliminary setup, sandboxing with current data. Looking for trends, examples, etc. 
+
+"""
+
+# preamble
+import pdb
+import numpy as np
+from scipy.interpolate import interp1d
+import datetime as dt
+import matplotlib.pyplot as plt
+import cryptoplayground as cp
+
+# calculate statistics 
+s
